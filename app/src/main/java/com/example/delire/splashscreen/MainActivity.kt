6d0916547c25b710a3e93d42d5c15d626a64a68b
FelixDallaire <felix.dr01@gmail.com>
2022-03-04 17:25:37 -0500

    added mockup of the filter section-> could be modified for the darker underlayout

--- app/src/main/java/com/example/delire/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/delire/splashscreen/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.delire.splashscreen
 
-import android.content.Context
-import android.graphics.Color
 import kotlinx.android.synthetic.main.activity_main.view.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.delire.R
 import kotlinx.android.synthetic.main. activity_main.*
@@ -25,6 +24,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var partnerSlots = arrayOf(
+            matchingDot1,
+            matchingDot2,
+            matchingDot3,
+            matchingDot4,
+            matchingDot5,
+            matchingDot6,
+            matchingDot7,
+            matchingDot8,
+            matchingDot9,)
 
         gymButton1.setOnClickListener{
             fillpatnersSlots();
@@ -56,6 +66,57 @@ class MainActivity : AppCompatActivity() {
             gymButton3.setTextColor(ContextCompat.getColor(this, R.color.black));
         }
 
+        filterButton.setOnClickListener{
+            var partnerSlots = arrayOf(
+                matchingDot1,
+                matchingDot2,
+                matchingDot3,
+                matchingDot4,
+                matchingDot5,
+                matchingDot6,
+                matchingDot7,
+                matchingDot8,
+                matchingDot9,)
+
+            var filterViewvisibility = filterView.visibility;
+
+            if (filterViewvisibility == View.GONE){
+                filterView.visibility = View.VISIBLE
+                partnerView.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black_transparent_bg));
+                for (slot in partnerSlots){
+                    slot.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black_transparent_bg));
+                    slot.alpha = 0f
+                }
+            }else {
+                filterView.visibility = View.GONE
+                partnerView.setBackgroundTintList(null);
+                for (slot in partnerSlots){
+                    slot.setBackgroundTintList(null);
+                    slot.alpha = 1f
+                }
+            }
+        }
+
+        closeFilterButton.setOnClickListener{
+            var partnerSlots = arrayOf(
+                matchingDot1,
+                matchingDot2,
+                matchingDot3,
+                matchingDot4,
+                matchingDot5,
+                matchingDot6,
+                matchingDot7,
+                matchingDot8,
+                matchingDot9,)
+
+
+            filterView.visibility = View.GONE
+            partnerView.setBackgroundTintList(null);
+            for (slot in partnerSlots){
+                slot.setBackgroundTintList(null);
+                slot.alpha = 1f
+            }
+        }
     }
 
     public fun fillpatnersSlots(){
