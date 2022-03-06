49615141e7ab0bada930cef09440542cbd2da8cd
FelixDallaire <felix.dr01@gmail.com>
2022-03-06 11:00:12 -0500

    added partners class

--- app/src/main/java/com/example/delire/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/delire/splashscreen/MainActivity.kt
@@ -1,31 +1,40 @@
 package com.example.delire.splashscreen
 
+import android.media.Image
 import kotlinx.android.synthetic.main.activity_main.view.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import com.example.delire.R
+import com.mikhaellopez.circularimageview.CircularImageView
 import kotlinx.android.synthetic.main. activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var partnerProfilePic = listOf(
-        R.drawable.profile_picture1,
-        R.drawable.profile_picture2,
-        R.drawable.profile_picture3,
-        R.drawable.profile_picture4,
-        R.drawable.profile_picture5,
-        R.drawable.profile_picture6,
-        R.drawable.profile_picture7,
-        R.drawable.profile_picture8,
-        R.drawable.profile_picture9,)
+    var partnerSlots = arrayOf<CircularImageView>();
+
+    var user1 = Partners(R.drawable.profile_picture1, "Elsiiie", "Elsie", "Pearson", "",true, false)
+    var user2 = Partners(R.drawable.profile_picture2, "Dydy", "Dylan", "Anderson", "",true, true)
+    var user3 = Partners(R.drawable.profile_picture3, "Emery", "Emery", "Misenar", "",false, false)
+    var user4 = Partners(R.drawable.profile_picture4, "Tommy", "Tom", "Keel", "",true, true)
+    var user5 = Partners(R.drawable.profile_picture5, "Angus", "Angelo", "Gustas", "",false, false)
+    var user6 = Partners(R.drawable.profile_picture6, "Maesher", "Mae", "Sherrer", "",true, true)
+    var user7 = Partners(R.drawable.profile_picture7, "Luperillion", "Lupe", "Tarrillion", "",true, false)
+    var user8 = Partners(R.drawable.profile_picture8, "Zar", "Zarda", "Deziel", "",true, true)
+    var user9 = Partners(R.drawable.profile_picture9, "ArtL", "Art", "Leers", "",true, false)
+    var user10 = Partners(R.drawable.profile_picture10, "Samtrox", "Samual", "Troxler", "",true, false)
+
+    private var userList = listOf(
+        user1, user2, user3, user4, user5, user6, user7, user8, user9, user10
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var partnerSlots = arrayOf(
+        partnerSlots = arrayOf(
             matchingDot1,
             matchingDot2,
             matchingDot3,
@@ -67,17 +76,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         filterButton.setOnClickListener{
-            var partnerSlots = arrayOf(
-                matchingDot1,
-                matchingDot2,
-                matchingDot3,
-                matchingDot4,
-                matchingDot5,
-                matchingDot6,
-                matchingDot7,
-                matchingDot8,
-                matchingDot9,)
-
             var filterViewvisibility = filterView.visibility;
 
             if (filterViewvisibility == View.GONE){
@@ -98,18 +96,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         closeFilterButton.setOnClickListener{
-            var partnerSlots = arrayOf(
-                matchingDot1,
-                matchingDot2,
-                matchingDot3,
-                matchingDot4,
-                matchingDot5,
-                matchingDot6,
-                matchingDot7,
-                matchingDot8,
-                matchingDot9,)
-
-
             filterView.visibility = View.GONE
             partnerView.setBackgroundTintList(null);
             for (slot in partnerSlots){
@@ -120,22 +106,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     public fun fillpatnersSlots(){
-
-        var partnerSlots = arrayOf(
-            matchingDot1,
-            matchingDot2,
-            matchingDot3,
-            matchingDot4,
-            matchingDot5,
-            matchingDot6,
-            matchingDot7,
-            matchingDot8,
-            matchingDot9,)
-
         for (slot in partnerSlots){
-            slot.setImageResource(partnerProfilePic.random())
+            slot.setImageResource(userList.random().profilePicture)
         }
     }
+}
+
+class Partners(
+    val profilePicture: Int,
+    val username: String,
+    val firstname: String,
+    val lastName: String,
+    val middleName: String,
+    val leadCertified: Boolean,
+    val topRopeCertified: Boolean
+    ){
+
 }
 
 
